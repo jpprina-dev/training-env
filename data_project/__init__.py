529fada73b925187193c07428352925cb586caaf
Juan Prina <c.jprin@ternium.com.br>
2024-05-09 14:38:17 -0300

    add date and coin_id arguments

--- data_project/__init__.py
+++ data_project/__init__.py
@@ -1,1 +1,3 @@
 from data_project.main import main
+
+__version__ = "0.1.0"
--- data_project/main.py
+++ data_project/main.py
@@ -1,8 +1,26 @@
 import click
+from . import __version__
+from datetime import datetime
+
+def datetime_valid(ctx, param, value):
+    """ Date in ISO Format Validation """
+    try:
+        # Validation required because --date_end is not a required option
+        if value:
+            date = datetime.fromisoformat(value)
+            return date
+        else:
+            return None
+    except ValueError:
+        click.BadParameter("date must be in ISO8601 format")
 
 
 @click.command()
 @click.option('--hello', prompt=True)
+@click.option('--date', required=True, type=str, callback=datetime_valid,
+              help='ISO8601 format date')
+@click.version_option(version=__version__)
+@click.argument('coin_id')
 def main(hello):
     if hello:
         click.echo(f'Hello {hello}!')
